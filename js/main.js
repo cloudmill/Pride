11dfaa7a8513a9481f9d7820f5d144fe51be978b
CloudMill Digital Production <34512874+cloudmill@users.noreply.github.com>
2019-12-23 13:04:39 +0300

    header desctop

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+  header.init();
+})
+
+header = {
+  scrolling:function(scroll){
+    if(scroll>0){
+      $('.header').addClass('scrolled')
+    }else{
+      $('.header').removeClass('scrolled')
+    }
+  },
+  events:function(){
+    var _this = this;
+    $(document).on('scroll',function(){
+      _this.scrolling($(document).scrollTop());
+    })
+  },
+  init:function(){
+    this.events();
+  }
+}
